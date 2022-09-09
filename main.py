d657af04833839c407b40dab8c7ee6fd31f3b891
Pau Lovell <pauxcode@gmail.com>
2022-09-08 21:25:46 -0500

    Main optimized

--- main.py
+++ main.py
@@ -1,56 +1,66 @@
+import shutil
 from os import walk
 from PIL import Image
-import shutil
-
-# * Referencia de rutas
-#rootPath = '/home/najimi/Ignite/Images/Anime/'
-#finalPath = '/home/najimi/Ignite/Images/Wallpapers/'
-
-# ? Tal vez pasar las constantes a otro archivo
-HD = [1280, 720]
-FULL_HD = [1920, 1080]
-ULTRA_HD = [3840, 2160]
-CUATRO_K = [4096, 2160]
-OCHO_K = [7680, 4320]
-
-QUALITIES = (HD, FULL_HD, ULTRA_HD, CUATRO_K, OCHO_K)
+from screen_resolutions import resolutions
 
 
-def run(originPath, destinyPath, quality, op_recursive):
-    # Todo: Imprimir los ASCII
-    emptyDir = 0
-    # ? Mejores nombres xd
-    quality_width, quality_height = quality
-    for (root, dirs, files) in walk(originPath, topdown=op_recursive):
+# ? Make a list of excluded folders
+
+# * Route reference
+# rootPath = '/home/najimi/Ignite/Images/Anime/'
+# finalPath = '/home/najimi/Ignite/Images/Wallpapers/'
+
+def menu():
+    origin_path = str(input('Give me the origin path to search: '))
+    destiny_path = str(input('Give me the destiny path: '))
+    resolution = int(input('What quality do you want to look for?\n'
+                           '1) HD+\n'
+                           '2) Full HD+\n'
+                           '3) Ultra HD+\n'
+                           '4) 4K+\n'
+                           '5) 8K+\n'))
+
+    # TODO: Search how to have default routes
+    searcher(origin_path, destiny_path, resolutions[resolution - 1])
+
+
+def searcher(origin_path, destiny_path, resolution):
+    # Todo: Print ASCII
+    empty_dir = 0
+    files_found = 0
+    resolution_width, resolution_height = resolution
+
+    for (root, dirs, files) in walk(origin_path, topdown=True):
         print(f'\n\n\033[0;44mSearching in: {root}\033[0;0m')
 
         for img in files:
-            imgPath = f'{root}/{img}'
-            with Image.open(imgPath) as image:
+            img_path = f'{root}/{img}'
+            with Image.open(img_path) as image:
                 width, height = image.size
-                if width >= quality_width and height >= quality_height and width > height:
-                    shutil.copy(imgPath, f'{destinyPath}{img}')
-                    # TODO: Hacer un contador de imagenes encontradas
-                    # ? Deberia dejar como esta??
-                    emptyDir = True
-                    print(
-                        f'\033[0;92mThe Image:\033[0;0m {imgPath} \033[0;92mwas copied to:\033[0;0m {destinyPath}')
+                if width >= resolution_width and resolution_height <= height < width:
+                    # width >= quality_width and height >= quality_height and width > height:
+                    shutil.copy(img_path, f'{destiny_path}{img}')
+                    # ? Should I quit?
+                    empty_dir = True
+                    files_found += 1
+                    print(f'\033[0;92mThe Image:\033[0;0m {img_path} \033[0;92mwas copied to:\033[0;0m {destiny_path}')
 
-        if emptyDir == 0:
+        if empty_dir == 0:
             print(f'\033[0;91mNot image found\033[0;0m')
 
-        emptyDir = 0
+        empty_dir = 0
+
+    print(f'\n\nTotal images found: \033[0;92m{files_found}\033[0;0m\n\n')
 
 
 if __name__ == '__main__':
-    # * Hacer de esto un menu
-    originPath = str(input('Give me the origin path to search: '))
-    recursive = str(input('You want to search recursively? (Y/n) '))
-    # TODO: Checar que funcione con 'y' minuscula y ponerler un mejor nombre xd
-    op_recursive = True if recursive == 'Y' else False
-    destinyPath = str(input('Give me the destiny path: '))
-    quality = int(input(
-        'What quality do you want to look for\n1) HD+\n2) Full HD+\n3) Ultra HD+\n4) 4K+\n5) 8K+\n'))
+    while True:
+        print('\033[0;96mWelcome to my program to search for possible wallpapers\033[0;0m\n')
+        menu_op = str(input('Do you want to start? (Y/n): '))
 
-    # TODO: Buscar como tener rutas por defecto
-    run(originPath, destinyPath, QUALITIES[quality-1], op_recursive)
+        if menu_op == 'n':
+            print('Good bye, see you later')
+            break
+
+        menu()
+    # * Export libraries
