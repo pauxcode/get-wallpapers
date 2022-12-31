299e586ceda14d09642fd9b26bb178b57837d779
Bridget <pauxcode@gmail.com>
2022-12-30 18:31:38 -0600

    Modify entries

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ from screen_resolutions import resolutions
 # ? Make a list of excluded folders
 
 # * Route reference
-# rootPath = '/home/najimi/Ignite/Images/Anime/'
-# finalPath = '/home/najimi/Ignite/Images/Wallpapers/'
+# rootPath = '/home/bridget/Ignite/Images/Anime/'
+# finalPath = '/home/bridget/Ignite/Images/Wallpapers/Maybe/'
 
 def menu():
-    origin_path = str(input('Give me the origin path to search: '))
-    destiny_path = str(input('Give me the destiny path: '))
+    origin_path = str(input('Give me the path to search: '))
+    destiny_path = str(input('Give me the path to save: '))
     resolution = int(input('What quality do you want to look for?\n'
-                           '1) HD+\n'
-                           '2) Full HD+\n'
-                           '3) Ultra HD+\n'
-                           '4) 4K+\n'
-                           '5) 8K+\n'))
+                           '1) HD or higher\n'
+                           '2) Full HD or higher\n'
+                           '3) Ultra HD or higher\n'
+                           '4) 4K or higher\n'
+                           '> '))
 
     # TODO: Search how to have default routes
     searcher(origin_path, destiny_path, resolutions[resolution - 1])
@@ -34,16 +34,19 @@ def searcher(origin_path, destiny_path, resolution):
         print(f'\n\n\033[0;44mSearching in: {root}\033[0;0m')
 
         for img in files:
-            img_path = f'{root}/{img}'
-            with Image.open(img_path) as image:
-                width, height = image.size
-                if width >= resolution_width and resolution_height <= height < width:
-                    # width >= quality_width and height >= quality_height and width > height:
-                    shutil.copy(img_path, f'{destiny_path}{img}')
-                    # ? Should I quit?
-                    empty_dir = True
-                    files_found += 1
-                    print(f'\033[0;92mThe Image:\033[0;0m {img_path} \033[0;92mwas copied to:\033[0;0m {destiny_path}')
+            try:
+                img_path = f'{root}/{img}'
+                with Image.open(img_path) as image:
+                    width, height = image.size
+                    if width >= resolution_width and resolution_height <= height < width:
+                        # width >= quality_width and height >= quality_height and width > height:
+                        shutil.copy(img_path, f'{destiny_path}{img}')
+                        # ? Should I quit?
+                        empty_dir = True
+                        files_found += 1
+                        print(f'\033[0;92mThe Image:\033[0;0m {img_path} \033[0;92mwas copied to:\033[0;0m {destiny_path}')
+            except OSError as err:
+                print(f'Error: {err}')
 
         if empty_dir == 0:
             print(f'\033[0;91mNot image found\033[0;0m')
