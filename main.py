a1065eb2cf9af87d2f06d4a6faa3878011aa896a
Emily <pauxcode@gmail.com>
2023-09-20 16:17:16 -0600

    Programa optimizado y con argumentos de linea de comando agregados

--- main.py
+++ main.py
@@ -1,69 +1,51 @@
 import shutil
-from os import walk
+import os
 from PIL import Image
 from screen_resolutions import resolutions
-
+from directories import origin_path, destiny_path
+from argparse_config import args
 
 # ? Make a list of excluded folders
 
-# * Route reference
-# rootPath = '/home/bridget/Ignite/Images/Anime/'
-# finalPath = '/home/bridget/Ignite/Images/Wallpapers/Maybe/'
-
-def menu():
-    origin_path = str(input('Give me the path to search: '))
-    destiny_path = str(input('Give me the path to save: '))
-    resolution = int(input('What quality do you want to look for?\n'
-                           '1) HD or higher\n'
-                           '2) Full HD or higher\n'
-                           '3) Ultra HD or higher\n'
-                           '4) 4K or higher\n'
-                           '> '))
-
-    # TODO: Search how to have default routes
-    searcher(origin_path, destiny_path, resolutions[resolution - 1])
-
-
 def searcher(origin_path, destiny_path, resolution):
-    # Todo: Print ASCII
-    empty_dir = 0
     files_found = 0
     resolution_width, resolution_height = resolution
 
-    for (root, dirs, files) in walk(origin_path, topdown=True):
+    for (root, dirs, files) in os.walk (origin_path, topdown=True):
+        empty_dir = True
         print(f'\n\n\033[0;44mSearching in: {root}\033[0;0m')
-
         for img in files:
             try:
                 img_path = f'{root}/{img}'
                 with Image.open(img_path) as image:
-                    width, height = image.size
-                    if width >= resolution_width and resolution_height <= height < width:
-                        # width >= quality_width and height >= quality_height and width > height:
-                        shutil.copy(img_path, f'{destiny_path}{img}')
-                        # ? Should I quit?
-                        empty_dir = True
+                    if image.width >= resolution_width and resolution_height <= image.height < image.width:
+                        shutil.copy(img_path, f'{destiny_path}')
                         files_found += 1
+                        empty_dir = False
                         print(f'\033[0;92mThe Image:\033[0;0m {img_path} \033[0;92mwas copied to:\033[0;0m {destiny_path}')
             except OSError as err:
+                print(f'\033[0;91mError: {err}\033[0;0m')
                 print(f'Error: {err}')
 
-        if empty_dir == 0:
-            print(f'\033[0;91mNot image found\033[0;0m')
-
-        empty_dir = 0
+        if empty_dir:
+            print(f'\033[0;91mNot images found\033[0;0m')
 
     print(f'\n\nTotal images found: \033[0;92m{files_found}\033[0;0m\n\n')
 
 
 if __name__ == '__main__':
-    while True:
-        print('\033[0;96mWelcome to my program to search for possible wallpapers\033[0;0m\n')
-        menu_op = str(input('Do you want to start? (Y/n): '))
+    resolution = None
 
-        if menu_op == 'n':
-            print('Good bye, see you later')
-            break
+    if args.quality.upper() == 'HD':
+        resolution = 1
+    elif args.quality.upper() == 'FHD':
+        resolution = 2
+    elif args.quality.upper() == '2K':
+        resolution = 3
+    elif args.quality.upper() == '4K':
+        resolution = 4
+    else:
+        raise ValueError(f'{args.quality.upper()} is not a valid option. See --help.')
 
-        menu()
-    # * Export libraries
+    searcher(origin_path, destiny_path, resolutions[resolution - 1])
+
