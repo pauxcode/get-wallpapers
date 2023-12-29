47ed097bd6361a105b25aa7ca825510326089203
Emily <pauxcode@gmail.com>
2023-12-29 16:42:25 -0600

    Modified main

--- main.py
+++ main.py
@@ -7,25 +7,32 @@ from argparse_config import args
 
 # ? Make a list of excluded folders
 
+def walk_generator(path, topdown=True):
+    for root, dirs, files in os.walk(path, topdown=topdown):
+        yield root, dirs, files
+
+def is_valid_image(image, resolution_width, resolution_height):
+    return image.width >= resolution_width and resolution_height <= image.height < image.width
+
+def copy_image(image_path, destination_path):
+    shutil.copy(image_path, destination_path)
+
 def searcher(origin_path, destiny_path, resolution):
     files_found = 0
     resolution_width, resolution_height = resolution
 
-    for (root, dirs, files) in os.walk (origin_path, topdown=True):
+    for root, dirs, files in walk_generator(origin_path):
         empty_dir = True
         print(f'\n\n\033[0;44mSearching in: {root}\033[0;0m')
         for img in files:
             try:
-                img_path = f'{root}/{img}'
-                with Image.open(img_path) as image:
-                    if image.width >= resolution_width and resolution_height <= image.height < image.width:
-                        shutil.copy(img_path, f'{destiny_path}')
-                        files_found += 1
-                        empty_dir = False
-                        print(f'\033[0;92mThe Image:\033[0;0m {img_path} \033[0;92mwas copied to:\033[0;0m {destiny_path}')
+                if is_valid_image(Image.open(f'{root}/{img}'), resolution_width, resolution_height):
+                    copy_image(f'{root}/{img}', f'destiny_path')
+                    files_found += 1
+                    empty_dir = False
+                    print(f'\033[0;92mThe Image:\033[0;0m {img} \033[0;92mwas copied to:\033[0;0m {destiny_path}')
             except OSError as err:
                 print(f'\033[0;91mError: {err}\033[0;0m')
-                print(f'Error: {err}')
 
         if empty_dir:
             print(f'\033[0;91mNot images found\033[0;0m')
